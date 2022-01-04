57dbd643fd62d86f77c8818c6a2769bfc96f3a3e
k0nekt <47927862+k0nekt@users.noreply.github.com>
2022-01-04 12:26:35 +0100

    Add files via upload
    Starting code (finished primitive anti-doublepost), no serious bug checking

---
+++ deploy/cloudFunctions.js
@@ -0,0 +1,43 @@
+const { default: Moralis } = require("moralis/types");
+
+//RETURNS TO DELETE = IF RETURN TRUE => DELETE, IF RETURN FALSE => DO NOT DELETE
+Moralis.Cloud.define("onMessage", async (request) => {
+    let logger = Moralis.Cloud.getLogger();
+    const userId = request.params.userId;
+    const currMsg = request.params.currentMessage;
+    const discordUsername = request.params.discordUsername;
+    const discriminator = request.params.discriminator;
+    
+    let dcQuery = new Moralis.Query("DiscordUsers");
+    dcQuery.equalTo("userId", userId);
+    let userResult = await dcQuery.find({useMasterKey: true});
+    let userExists = (userResult.length > 0) ? (true) : (false);
+
+    if (!userExists) {
+        let discordUser = Moralis.Object.extend("DiscordUsers");
+        let newUser = new discordUser();
+        newUser.set("lastMessage", currMsg);
+        newUser.set("userId", userId);
+        newUser.set("discordUsername", discordUsername);
+        newUser.set("discriminator", discriminator);
+        newUser.save(null, {useMasterKey: true});
+        return false;
+    } 
+
+    dcQuery.equalTo("userId", userId);
+    let msgResult = await dcQuery.find({useMasterKey: true});
+    let lastMsg = msgResult[0].attributes.lastMessage;
+
+    userResult[0].set("lastMessage", currMsg);
+    userResult[0].save(null, {useMasterKey: true});
+
+    if (lastMsg.length != currMsg.length) {
+        return false;
+    }
+    
+    if (lastMsg === currMsg) { 
+        return true;
+    } else {
+        return false;
+    }
+});
---
+++ deploy/main.js
@@ -0,0 +1,44 @@
+const TOKEN = process.env['TOKEN'];
+const APP_ID = process.env["APP_ID"];
+const SERVER_URL = process.env["SERVER_URL"];
+const Discord = require("discord.js"); 
+const client = new Discord.Client();
+const fetch = require("node-fetch");
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("message", async (msg) => {
+  if (msg.author.id != "926422385094168577" && !msg.author.bot && !msg.system){
+    let currentAuthor = msg.author;
+    let currentMessageString;
+    if (msg.content.length < 30) {
+        currentMessageString = msg.content.toLowerCase();
+    } else {
+        currentMessageString = msg.content.slice(0, 30).toLowerCase();
+    }
+
+    let params = {
+        userId: currentAuthor.id,
+        discordUsername: currentAuthor.username,
+        discriminator: currentAuthor.discriminator,
+        currentMessage: currentMessageString
+    };
+    
+    
+
+    //const channel = client.channels.cache.get(msg.channel.id);
+    let deleteCheck = await fetch(`${SERVER_URL}/functions/onMessage?_ApplicationId=${APP_ID}&userId=${params.userId}&currentMessage=${params.currentMessage}
+    &discriminator=${params.discriminator}&discordUsername=${params.discordUsername}`);
+    deleteCheck = await deleteCheck.json();
+    deleteCheck = deleteCheck.result;
+    if (deleteCheck) {
+      msg.delete();
+      currentAuthor.send("Please do not post the same message in multiple channels, thanks!");
+    }
+    console.log(`${currentAuthor.username}#${currentAuthor.discriminator}: ${currentMessageString} - ${deleteCheck}`);
+  }
+});
+
+client.login(TOKEN);
