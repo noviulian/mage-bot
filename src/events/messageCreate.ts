49dd26f1b117f2b31631178061277d853fd3ae8b
iuliannov <contact@iuliannov.com>
2023-02-04 23:42:57 +0200

    added prisma

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,5 +1,30 @@
 import { Event } from '../client/event';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 export default new Event('messageCreate', async (message) => {
-	console.log(message);
+	const users = await prisma.user.findMany();
+
+	//create a user if they don't exist
+	if (!users.find((user) => user.userId === message.author.id)) {
+		const user = await prisma.user.create({
+			data: {
+				username: message.author.username,
+				userId: message.author.id,
+				lastMessage: message.content,
+				joinedAt: message.guild.joinedAt,
+			},
+		});
+		console.log(user);
+	}
+
+	//update the user's last message
+	await prisma.user.update({
+		where: {
+			userId: message.author.id,
+		},
+		data: {
+			lastMessage: message.content,
+		},
+	});
 });
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,14 +1,20 @@
 import { ExtendedClient } from 'client/client';
 import 'colors';
-
 import { Event } from '../client/event';
+import { PrismaClient } from '@prisma/client';
 
-export default new Event('ready', (client: ExtendedClient) => {
+const prisma = new PrismaClient();
+
+export default new Event('ready', async (client: ExtendedClient) => {
 	console.log(
 		`${client.user.username}#${client.user.discriminator} is online and ready`
 			.green.bold,
 	);
 	client.guilds.cache.forEach((guild) => {
 		console.log(`Guild: ${guild.name}`.yellow.bold);
+	});
+
+	await prisma.$connect().then(() => {
+		console.log('Connected to database'.green.bold);
 	});
 });
